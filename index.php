2131726ef6233edb39bd2b34c42d7d35aacbdbf4
Clemy-beep <clementine.digny@gmail.com>
2022-01-14 12:42:04 +0100

    use less use removed

--- index.php
+++ index.php
@@ -2,12 +2,7 @@
 
 require 'bootstrap.php';
 use App\Controllers\AppController;
-use App\Entity\Airport;
-use App\Entity\Client;
-use App\Entity\Flight;
-use App\Entity\Passager;
-use App\Entity\Ticket;
-use Doctrine\Common\Collections\ArrayCollection;
+
 
 require __DIR__.'/vendor/autoload.php';
 
